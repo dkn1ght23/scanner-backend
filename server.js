5ea4cb84c463c58545d9677ac5f5996c2581a594
dkn1ght23 <mujammal.salman@gmail.com>
2025-03-31 01:48:37 -0400

    fix error router and geminai integration done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const scanRoutes = require("./routes/scanRoutes");
 const zapRoutes = require("./routes/zapRoutes");
+const fixRoutes = require("./routes/fixRoutes");
 
 const app = express();
 app.use(cors({ origin: "*" }));
@@ -10,6 +11,7 @@ app.use(express.json());
 // Routes
 app.use("/api", scanRoutes);
 app.use("/api", zapRoutes);
+app.use("/api", fixRoutes);
 
 // Start the server
 const PORT = 5000;
